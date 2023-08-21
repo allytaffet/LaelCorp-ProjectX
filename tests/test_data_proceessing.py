ca71b580eaba6892a3dd8067b900078a74d84320
Ally Taffet <142642596+allytaffet@users.noreply.github.com>
2023-08-20 19:44:24 -0500

    Create test_data_proceessing.py

---
+++ tests/test_data_proceessing.py
@@ -0,0 +1,14 @@
+import unittest
+from analytics.data_processing import load_data, process_data
+
+class TestDataProcessing(unittest.TestCase):
+    def test_load_data(self):
+        # Implementation of test cases for loading data
+        pass
+
+    def test_process_data(self):
+        # Implementation of test cases for processing data
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
