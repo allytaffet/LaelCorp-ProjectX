f5bdb5c60762a3785b55a72607b5a45b2110f554
Ally Taffet <142642596+allytaffet@users.noreply.github.com>
2023-08-20 19:45:17 -0500

    Create test_visualization.py

---
+++ tests/test_visualization.py
@@ -0,0 +1,14 @@
+import unittest
+from analytics.visualization import plot_histogram, plot_line_chart
+
+class TestVisualization(unittest.TestCase):
+    def test_plot_histogram(self):
+        # Implementation of test cases for plotting histograms
+        pass
+
+    def test_plot_line_chart(self):
+        # Implementation of test cases for plotting line charts
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
