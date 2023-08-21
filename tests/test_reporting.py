4bd1f88832d3dcd7ccac7f6e89398977be023812
Ally Taffet <142642596+allytaffet@users.noreply.github.com>
2023-08-20 19:44:52 -0500

    Create test_reporting.py

---
+++ tests/test_reporting.py
@@ -0,0 +1,10 @@
+import unittest
+from analytics.reporting import generate_report
+
+class TestReporting(unittest.TestCase):
+    def test_generate_report(self):
+        # Implementation of test cases for generating reports
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
